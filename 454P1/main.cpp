cdc3c661644b40b03da3c8df0b0ab15805cf435c
student <student@ssu-cs-dar25-167-190.sonoma.edu>
2019-02-15 16:58:42 -0800

    Initial Commit

---
+++ 454P1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  454P1
+//
+//  Created by student on 2/15/19.
+//  Copyright © 2019 Will-Martin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
