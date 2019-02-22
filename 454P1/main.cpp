27ac995ed90272082553317fed2399cb72a74ea7
supersaiyansubtlety <supersaiyansubtlety@users.noreply.github.com>
2019-02-21 21:38:43 -0800

    2D array version of transitions

--- 454P1/main.cpp
+++ 454P1/main.cpp
@@ -7,107 +7,61 @@
 //
 
 #include <iostream>
-#include <map>
-#include <utility>
 #include <string>
 
 using namespace std;
 
-enum states {e,a,b,c,ab,ac,ba,bc,ca,cb,aa,bb,cc,abc,acb,bac,bca,cab,cba,aab,aba,abb,aac,aca,baa,bab,bba,bbc,bcb,caa,cac,cca,aaa,bbb,ccc,BAD};
+enum states {a, b, c, ab, ac, ba, bc, ca, cb, aa, bb, cc, abc, acb, bac, bca, cab, cba, aab, aba, abb, aac, aca, acc, baa, bab, bba, bbc, bcb, bcc, caa, cac, cca, cbb, cbc, ccb, aaa, bbb, ccc, BAD};
 
 string alphabet = "abc";
 
-int N(int length);
-
 int main(int argc, const char * argv[])
 {
-    map<pair<int, char>, int> transitions =
+    int transitions[41][3] =
     {
-        {{e,'a'},a},
-        {{a,'a'},aa},
-        {{b,'a'},ba},
-        {{c,'a'},ca},
-        {{ab,'a'},aba},
-        {{ac,'a'},aca},
-        {{ba,'a'},baa},
-        {{bc,'a'},bca},
-        {{ca,'a'},caa},
-        {{cb,'a'},cba},
-        {{aa,'a'},aaa},
-        {{bb,'a'},bba},
-        {{cc,'a'},cca},
-        {{abc,'a'},bca},
-        {{acb,'a'},cba},
-        {{bac,'a'},aca},
-        {{bca,'a'},caa},
-        {{cab,'a'},aba},
-        {{cba,'a'},baa},
-        {{aab,'a'},BAD},
-        {{aba,'a'},BAD},
-        {{abb,'a'},BAD},
-        {{aac,'a'},BAD},
-        {{aca,'a'},BAD},
-        {{baa,'a'},BAD},
-        {{bab,'a'},BAD},
-        {{bba,'a'},BAD},
-        {{bbc,'a'},BAD},
-        {{bcb,'a'},cba},
-        {{caa,'a'},BAD},
-        {{cac,'a'},BAD},
-        {{cca,'a'},BAD},
-        {{aaa,'a'},BAD},
-        {{bbb,'a'},BAD},
-        {{ccc,'a'},BAD},
-        {{BAD,'a'},BAD},
-        
-        {{e,'b'},b},
-        {{a,'b'},ab},
-        {{b,'b'},bb},
-        {{c,'b'},cb},
-        {{ab,'b'},abb},
-        {{ac,'b'},acb},
-        {{ba,'b'},bab},
-        {{bc,'b'},bcb},
-        {{ca,'b'},cab},
-        {{cb,'b'},cbb},
-        {{aa,'b'},aab},
-        {{bb,'b'},bbb},
-        {{cc,'b'},ccb},
-        {{abc,'b'},bcb},
-        {{acb,'b'},cbb},
-        {{bac,'b'},acb},
-        {{bca,'b'},cab},
-        {{cab,'b'},abb},
-        {{cba,'b'},bab},
-        {{aab,'b'},BAD},
-        {{aba,'b'},BAD},
-        {{abb,'b'},BAD},
-        {{aac,'b'},acb},
-        {{aca,'b'},cab},
-        {{baa,'b'},BAD},
-        {{bab,'b'},BAD},
-        {{bba,'b'},BAD},
-        {{bbc,'b'},BAD},
-        {{bcb,'b'},BAD},
-        {{caa,'b'},aab},
-        {{cac,'b'},acb},
-        {{cca,'b'},cab},
-        {{cbc,'b'},BAD},
-        {{ccb,'b'},BAD},
-        {{aaa,'b'},BAD},
-        {{bbb,'b'},BAD},
-        {{ccc,'b'},BAD},
-        {{BAD,'b'},BAD}
+        {a,b,c},
+        {aa,ab,ac},
+        {ba,bb,bc},
+        {ca,cb,cc},
+        {aba,abb,abc},
+        {aca,acb,acc},
+        {baa,bab,bac},
+        {bca,bcb,bcc},
+        {caa,cab,cac},
+        {cba,cbb,cbc},
+        {aaa,aab,aac},
+        {bba,bbb,bbc},
+        {cca,ccb,ccc},
+        {bca,bcb,bcc},
+        {cba,cbb,cbc},
+        {aca,acb,acc},
+        {caa,cab,cac},
+        {aba,abb,abc},
+        {baa,bab,bac},
+        {BAD,BAD,abc},
+        {BAD,BAD,bac},
+        {BAD,BAD,bbc},
+        {BAD,acb,BAD},
+        {BAD,cab,BAD},
+        {BAD,acb,BAD},
+        {BAD,BAD,aac},
+        {BAD,BAD,abc},
+        {BAD,BAD,bac},
+        {bca,BAD,BAD},
+        {cba,BAD,BAD},
+        {cca,BAD,BAD},
+        {BAD,aab,BAD},
+        {BAD,acb,BAD},
+        {BAD,cab,BAD},
+        {bba,BAD,BAD},
+        {bca,BAD,BAD},
+        {cba,BAD,BAD},
+        {BAD,BAD,BAD},
+        {BAD,BAD,BAD},
+        {BAD,BAD,BAD},
+        {BAD,BAD,BAD}
     };
     
-    int transitions[38][3]={};
-    
-    return 0;
-}
-
-
-int N(int length)
-{
     int currentCount[38];
     int nextCount[38];
     
@@ -120,7 +74,7 @@ int N(int length)
     
     
     
-    for (int st = 0; state < 38; state++)
+    for (int st = 0; st < 38; st++)
     {
         for (int let = 0; let < 3; let++)
         {
@@ -136,4 +90,6 @@ int N(int length)
         currentCount[i] = nextCount[i];
         nextCount[i] = 0;
     }
+    
+    return 0;
 }
